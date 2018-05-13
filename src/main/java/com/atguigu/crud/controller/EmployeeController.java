3687eabad9aee62c1604fcdfff977ecfc33ec72b
Teacher Yu <lab.deepmind@gmail.com>
2018-05-13 15:43:27 +0800

    fix errors.

--- src/main/java/com/atguigu/crud/controller/EmployeeController.java
+++ src/main/java/com/atguigu/crud/controller/EmployeeController.java
@@ -1,29 +1,23 @@
 package com.atguigu.crud.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.atguigu.crud.bean.Employee;
 import com.atguigu.crud.bean.Msg;
 import com.atguigu.crud.service.EmployeeService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 处理员工CRUD请求
@@ -43,7 +37,7 @@ public class EmployeeController {
 	 * 批量删除：1-2-3
 	 * 单个删除：1
 	 * 
-	 * @param id
+	 * @param ids
 	 * @return
 	 */
 	@ResponseBody
